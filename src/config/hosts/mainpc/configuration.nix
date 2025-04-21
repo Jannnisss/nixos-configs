8e929b96466c973e6297ead82592e90ca44773c7
Jannis <git@jannis.one>
2025-04-21 18:12:05 +0200

    added flake.nix added modules added hosts

---
+++ src/config/hosts/mainpc/configuration.nix
@@ -0,0 +1,245 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = false;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Remove GNOME Packages.
+  environment.gnome.excludePackages = with pkgs; [
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    eog         # image viewer
+    epiphany    # web browser
+    #gedit       # text editor
+    simple-scan # document scanner
+    #totem       # video player
+    yelp        # help viewer
+    evince      # document viewer
+    file-roller # archive manager
+    geary       # email client
+    #seahorse    # password manager
+    hitori # sudoku game
+    iagno # go game
+    tali # poker game
+    snapshot # camera
+
+    #gnome-calculator 
+    gnome-calendar 
+    gnome-characters 
+    #gnome-clocks 
+    gnome-contacts
+    gnome-font-viewer 
+    #gnome-logs 
+    gnome-maps 
+    gnome-music 
+    #gnome-photos 
+    #gnome-screenshot
+    #gnome-system-monitor 
+    gnome-weather 
+    #gnome-disk-utility 
+    gnome-connections
+    gnome-tour
+    gnome-software
+  ];
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "us";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.jannis = {
+    isNormalUser = true;
+    description = "Jannis";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Define home manager
+  #home-manager = {
+  #  extraSpecialArgs = {inherit inputs;};
+  #  users = {
+  #    "jannis" = import ./home.nix
+  #  }
+  #};
+  # Install Librewolf.
+  programs.firefox = {
+  enable = true;
+  package = pkgs.librewolf;
+  policies = {
+    DisableTelemetry = true;
+    DisableFirefoxStudies = true;
+    Preferences = {
+      #"privacy.resistFingerprinting" = false;
+    };
+    ExtensionSettings = {
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "gdpr@cavi.au.dk" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/consent-o-matic/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "@contain-facebook" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/facebook-container/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "sponsorBlocker@ajay.app" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "{9a3104a2-02c2-464c-b069-82344e5ed4ec}" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-no-translation/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
+  };
+};
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # GNOME Shell extensions
+    gnomeExtensions.grand-theft-focus
+    gnomeExtensions.appindicator
+    gnomeExtensions.fullscreen-avoider
+
+    gnome-tweaks
+    lutris
+    vesktop
+    spotify
+    r2modman
+    libreoffice
+    librewolf
+    gitFull
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+      ];
+     })
+  ];
+
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
